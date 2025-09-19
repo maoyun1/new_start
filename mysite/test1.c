c5d64c5344e0c1e6b5f6d8e60bb9591fa6de255b
maoyun1 <3156782490@qq.com>
2025-09-19 09:58:49 +0800

    success1.0

--- mysite/test1.c
+++ mysite/test1.c
@@ -5,19 +5,18 @@ int main()
     printf("请输入：(输入Quit退出)\n");   
     char input[100];
     scanf("%s",input);
-    while(strcmp(input,"Quit")!=0&&strcmp(input,"quit")!=0)
+    while(strcmp(input,"Quit")!=0)
     {
-        if(strcmp(input,"Dian"))
+        if(strcmp(input,"Dian")==0)
         {
             printf("2002\n");
-            while(getchar()!='\n');
         }
         else 
         {
             printf("Error\n");
-            while(getchar()!='\n');
         }
          printf("请输入：(输入Quit退出)\n");
+         scanf("%s",input);
     }
     return 0;
 }
